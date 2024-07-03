b8365b5a41e976da7a482b76bac07d4695ba3670
Sidre Nil Denge <dengesidrenil@gmail.com>
2024-07-03 17:53:09 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <Site-navigation />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import SiteNavigation from "./components/SiteNavigation.vue";
+import { RouterView } from "vue-router";
+</script>
+
+<style lang="scss" scoped></style>
