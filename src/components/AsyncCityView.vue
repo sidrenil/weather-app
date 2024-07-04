a6f13477b04a961e7b9c58e3f9026a1ea485f136
Sidre Nil Denge <dengesidrenil@gmail.com>
2024-07-04 17:59:02 +0300

    CityViewLayout

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -25,9 +25,82 @@
           })
         }}
       </p>
+      <p class="text-8xl mb-8">
+        {{ Math.round(weatherData.current.temp) }} &deg;
+      </p>
+      <div class="text-center">
+        <p>
+          Feels like
+          {{ Math.round(weatherData.current.feels_like) }} &deg;
+        </p>
+        <p class="capitalize">
+          {{ weatherData.current.weather[0].description }}
+        </p>
+        <img
+          class="w-[150px] h-auto"
+          :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`"
+          alt=""
+        />
+      </div>
     </div>
     <div v-else class="flex flex-col items-center text-white py-12">
       <p>Loading...</p>
+    </div>
+    <hr class="border-white border-opacity-10 border w-full" />
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">Hourly Weather</h2>
+        <div class="flex gap-10 overflow-x-scroll">
+          <div
+            v-for="hourData in weatherData.hourly"
+            :key="hourData.dt"
+            class="flex flex-col gap-4 items-center"
+          >
+            <p class="whitespace-nowrap text-md">
+              {{
+                new Date(hourData.currentTime).toLocaleTimeString("en-us", {
+                  hour: "numeric",
+                })
+              }}
+            </p>
+            <img
+              class="w-auto h-[50px] object-cover"
+              :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
+              alt=""
+            />
+            <p class="text-xl">{{ Math.round(hourData.temp) }}&deg;</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <hr class="border-white border-opacity-10 border w-full" />
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">7 Day For Forecast</h2>
+        <div
+          v-for="day in weatherData.daily"
+          :key="day.dt"
+          class="flex items-center"
+        >
+          <p class="flex-1">
+            {{
+              new Date(day.dt * 1000).toLocaleDateString("en-us", {
+                weekday: "long",
+              })
+            }}
+          </p>
+          <img
+            class="w-[50px] h-[50px] object-cover"
+            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+            alt=""
+          />
+          <div class="flex gap-2 flex-1 justify-end">
+            <p>H: {{ Math.round(day.temp.max) }}</p>
+            <p>L: {{ Math.round(day.temp.min) }}</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -54,7 +127,9 @@ const getWeatherData = async () => {
       const utc = hour.dt * 1000 + localOffset;
       hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
     });
-    weatherData.value = weatherData.data;
+
+    return weatherData.data;
+    // weatherData.value = weatherData.data;
   } catch (err) {
     console.log("Error fetching weather data:", err);
   }
