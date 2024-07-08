7560d4a994c59b8b16bced68a2565eccff95d3e9
Sidre Nil Denge <dengesidrenil@gmail.com>
2024-07-08 15:04:59 +0300

    degree status

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="flex flex-col flex-1 items-center">
+    <!--Banner-->
     <div
       v-if="route.query.preview"
       class="text-white p-4 bg-weather-secondary w-full text-center"
@@ -9,131 +10,48 @@
         tracking this city.
       </p>
     </div>
-    <div v-if="weatherData" class="flex flex-col items-center text-white py-12">
+    <!--Weather Overview-->
+    <div class="flex flex-col items-center text-white py-12">
       <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
-      <p class="text-sm mb-12">
-        {{
-          new Date(weatherData.currentTime).toLocaleDateString("en-us", {
-            weekday: "short",
-            day: "2-digit",
-            month: "long",
-          })
-        }}
-        {{
-          new Date(weatherData.currentTime).toLocaleDateString("en-us", {
-            timeStyle: "short",
-          })
-        }}
+      <p v-if="weatherData" class="text-4xl mb-2">
+        {{ Math.round(weatherData.current.temp) }}&deg;
       </p>
-      <p class="text-8xl mb-8">
-        {{ Math.round(weatherData.current.temp) }} &deg;
-      </p>
-      <div class="text-center">
-        <p>
-          Feels like
-          {{ Math.round(weatherData.current.feels_like) }} &deg;
-        </p>
-        <p class="capitalize">
-          {{ weatherData.current.weather[0].description }}
-        </p>
-        <img
-          class="w-[150px] h-auto"
-          :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`"
-          alt=""
-        />
-      </div>
-    </div>
-    <div v-else class="flex flex-col items-center text-white py-12">
-      <p>Loading...</p>
-    </div>
-    <hr class="border-white border-opacity-10 border w-full" />
-    <div class="max-w-screen-md w-full py-12">
-      <div class="mx-8 text-white">
-        <h2 class="mb-4">Hourly Weather</h2>
-        <div class="flex gap-10 overflow-x-scroll">
-          <div
-            v-for="hourData in weatherData.hourly"
-            :key="hourData.dt"
-            class="flex flex-col gap-4 items-center"
-          >
-            <p class="whitespace-nowrap text-md">
-              {{
-                new Date(hourData.currentTime).toLocaleTimeString("en-us", {
-                  hour: "numeric",
-                })
-              }}
-            </p>
-            <img
-              class="w-auto h-[50px] object-cover"
-              :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
-              alt=""
-            />
-            <p class="text-xl">{{ Math.round(hourData.temp) }}&deg;</p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <hr class="border-white border-opacity-10 border w-full" />
-    <div class="max-w-screen-md w-full py-12">
-      <div class="mx-8 text-white">
-        <h2 class="mb-4">7 Day For Forecast</h2>
-        <div
-          v-for="day in weatherData.daily"
-          :key="day.dt"
-          class="flex items-center"
-        >
-          <p class="flex-1">
-            {{
-              new Date(day.dt * 1000).toLocaleDateString("en-us", {
-                weekday: "long",
-              })
-            }}
-          </p>
-          <img
-            class="w-[50px] h-[50px] object-cover"
-            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
-            alt=""
-          />
-          <div class="flex gap-2 flex-1 justify-end">
-            <p>H: {{ Math.round(day.temp.max) }}</p>
-            <p>L: {{ Math.round(day.temp.min) }}</p>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 import axios from "axios";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
 const weatherData = ref(null);
 
-const getWeatherData = async () => {
+const getWeatherData = async (id) => {
   try {
-    const result = await axios.get(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&appid=63501a7cd88c64a5b9533c246bfc06de&units=imperial`
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=3659b7a8b5e7572358698ad695eddfb9&units=metric`
     );
-    const localOffset = new Date().getTimezoneOffset() * 60000;
-    const utc = weatherData.data.current.dt * 1000 + localOffset;
-    weatherData.data.currentTime =
-      utc + 1000 * weatherData.data.timezone_offset;
+    console.log("baty", "response.data");
 
-    weatherData.data.hourly.forEach((hour) => {
+    const data = response.data;
+    const localOffset = new Date().getTimezoneOffset() * 60000;
+    const utc = data.current.dt * 1000 + localOffset;
+    data.currentTime = utc + 1000 * data.timezone_offset;
+
+    data.hourly.forEach((hour) => {
       const utc = hour.dt * 1000 + localOffset;
-      hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
+      hour.currentTime = utc + 1000 * data.timezone_offset;
     });
 
-    return weatherData.data;
-    // weatherData.value = weatherData.data;
+    weatherData.value = data; // weatherData'yı güncelleyin
   } catch (err) {
-    console.log("Error fetching weather data:", err);
+    console.error("Hava durumu verisi alınırken hata oluştu:", err);
   }
 };
 
-getWeatherData();
+onMounted(async () => {
+  await getWeatherData();
+});
 </script>
