ddad6a197002062a2ffa76b9433027bfbb76e6fb
Sidre Nil Denge <dengesidrenil@gmail.com>
2024-07-09 12:02:21 +0300

    title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <Site-navigation />
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Transition name="page" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </div>
 </template>
 
@@ -10,4 +14,14 @@ import SiteNavigation from "./components/SiteNavigation.vue";
 import { RouterView } from "vue-router";
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: 600ms ease all;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
